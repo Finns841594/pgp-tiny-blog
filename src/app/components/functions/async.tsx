06dff7f470b449bf4780ddb50d0627ce93b88a9a
Finns841594 <fengy142857@gmail.com>
2023-05-25 15:33:48 +0200

    fix domain problem

--- src/app/components/functions/async.tsx
+++ src/app/components/functions/async.tsx
@@ -1,17 +1,18 @@
 // get current host name
-const host = window.location.hostname
-const port = window.location.port
+// Update: method below is not working for the client side
+// const host = window.location.hostname
+// const port = window.location.port
 
 // function that get posts from api
 export const getPosts = async () => {
-  const res = await fetch(`http://${host}:${port}/api/posts`)
+  const res = await fetch(`/api/posts`)
   const posts = await res.json()
   return posts.posts
 }
 
 // function that get posts from api
 export const getPostsByLabel = async (label:string) => {
-  const res = await fetch(`http://${host}:${port}/api/posts?tag=${label}`)
+  const res = await fetch(`/api/posts?tag=${label}`)
   const posts = await res.json()
   return posts.posts
 }
