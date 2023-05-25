4673f3b7e9ce112a366115c20ca200c1d0cc6b1c
Finns841594 <fengy142857@gmail.com>
2023-05-25 09:44:36 +0200

    bf code review

--- src/app/components/BlogPost.tsx
+++ src/app/components/BlogPost.tsx
@@ -6,7 +6,7 @@ export const BlogPost = ({post}:Props) => {
   return (
     <div 
     key={post.id}
-    className='w-80 h-48 flex-none hover:h-auto hover:transition transition duration-300 ease-in-out overflow-y-hidden rounded-lg shadow hover:shadow-lg hover:shadow-violet-500 p-2 m-2'>
+    className='w-80 h-48 flex-none hover:h-auto hover:transition duration-300 ease-in-out overflow-y-hidden rounded-lg shadow-lg hover:shadow-lg hover:shadow-violet-500 p-2 m-2'>
       <h3 className='border-b-2 border-b-violet-600 py-2 font-bold'>{post.title}</h3>
       <p className='font-light text-sm my-2'>{post.body}</p>
       <div className='flex flex-row pb-2 text-sm '>
