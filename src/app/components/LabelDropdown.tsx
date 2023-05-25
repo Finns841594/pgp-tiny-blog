3dcb1197f0813355370e94674b5e726546b4b39a
Finns841594 <fengy142857@gmail.com>
2023-05-25 15:09:42 +0200

    fix type problem to deploy

--- src/app/components/LabelDropdown.tsx
+++ src/app/components/LabelDropdown.tsx
@@ -4,13 +4,14 @@ import { getAllLabels } from "./functions/utilities"
 import { Dropdown } from "@nextui-org/react";
 
 interface Props {updateLabel: (label:string) => void, defaultLabel:string}
+type MenuItem = {name:string, key:string}
 
 export const LabelDropdown = ({updateLabel, defaultLabel}:Props) => {
   const { posts } = useContext(PostsContext)
   const [selectedLabel, setSelectedLabel] = useState(defaultLabel)
 
   const labels = getAllLabels(posts)
-  const menuItems = labels.map((label) => ({
+  const menuItems:MenuItem[] = labels.map((label) => ({
     name: label,
     key: label,
   }))
@@ -31,14 +32,22 @@ export const LabelDropdown = ({updateLabel, defaultLabel}:Props) => {
       <h3 className="text-2xl mr-4 text-violet-800">Showing posts with label</h3>
       <Dropdown> 
       <Dropdown.Button shadow color='secondary' css={{ tt: "capitalize" }}>{selectedLabel}</Dropdown.Button>
-      <Dropdown.Menu aria-label="Dynamic Actions" css={{ tt: "capitalize" }} items={menuItems} selectionMode="single" selectedKeys={selectedLabel} onAction={ (key:Key) => selectHandler(key)}>
+      <Dropdown.Menu aria-label="Dynamic Actions" css={{ tt: "capitalize" }} selectionMode="single" selectedKeys={selectedLabel} onAction={ (key:Key) => selectHandler(key)}>
+        {labels.map((label) => (
+          <Dropdown.Item key={label} color={"default"}>
+            {label}
+          </Dropdown.Item>
+        ))}
+      </Dropdown.Menu>
+      {/* code snippet below is the original code, but it has problem with the type 'object', to be solved. */}
+      {/* <Dropdown.Menu aria-label="Dynamic Actions" css={{ tt: "capitalize" }} items={menuItems} selectionMode="single" selectedKeys={selectedLabel} onAction={ (key:Key) => selectHandler(key)}>
         {(item) => (
           // Its reporting error here that key doesn't exist on type 'object', but its working fine
           <Dropdown.Item key={item.key} color={"default"}>
             {item.name}
           </Dropdown.Item>
         )}
-      </Dropdown.Menu>
+      </Dropdown.Menu> */}
       </Dropdown>
     </div>
   )
